d43df073954df4c95632a565d8a67549bc158007
mnorval <mnorval@gmail.com>
2025-06-26 14:53:09 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,36 @@
 import os
-import http.server
-import socketserver
+from flask import Flask, Response, request
+from queue import Queue
+from threading import Lock
 
-from http import HTTPStatus
+app = Flask(__name__)
 
+audio_queue = Queue()
+queue_lock = Lock()
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+@app.route('/')
+def index():
+    return "SOS Audio Streaming Server is Running"
 
+@app.route('/audio', methods=['GET'])
+def audio_feed():
+    def stream():
+        while True:
+            chunk = audio_queue.get()
+            if chunk:
+                yield chunk
 
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+    return Response(stream(), mimetype='audio/mpeg')
+
+@app.route('/upload', methods=['POST'])
+def receive_audio():
+    chunk = request.data
+    if chunk:
+        with queue_lock:
+            audio_queue.put(chunk)
+        return "Chunk received", 200
+    return "No data", 400
+
+if __name__ == '__main__':
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, threaded=True)
